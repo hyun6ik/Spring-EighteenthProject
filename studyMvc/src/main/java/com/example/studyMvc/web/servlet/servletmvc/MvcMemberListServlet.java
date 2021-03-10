67c543b4af4b99b1676a3fae1d2e24bde67bf430
hyun6ik <fkanwm@naver.com>
2021-03-10 22:54:25 +0900

    MVC 패턴 - 적용 : JSTL 인식 못하는 오류 발생 ㅜㅜ

---
+++ studyMvc/src/main/java/com/example/studyMvc/web/servlet/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,33 @@
+package com.example.studyMvc.web.servlet.servletmvc;
+
+import com.example.studyMvc.domain.member.Member;
+import com.example.studyMvc.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+@RequiredArgsConstructor
+public class MvcMemberListServlet extends HttpServlet {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ studyMvc/src/main/webapp/WEB-INF/views/members.jsp
@@ -0,0 +1,26 @@
+<%@ page isELIgnored="false" contentType = "text/html; charset=euc-kr" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<a href="/index.html">¸ŞÀÎ</a>
+<table>
+    <thead>
+    <th>id</th>
+    <th>username</th>
+    <th>age</th>
+    </thead>
+    <tbody>
+    <c:forEach var="item" items="${members}">
+        <tr>
+            <td>${item.id}</td>
+            <td>${item.username}</td>
+            <td>${item.age}</td>
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
+</body>
+</html>
