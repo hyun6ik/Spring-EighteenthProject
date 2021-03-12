bf7d57648d9b1f7334564042881debdcb86d0629
hyun6ik <fkanwm@naver.com>
2021-03-12 17:37:55 +0900

    View 분리 - V2

---
+++ studyMvc/src/main/java/com/example/studyMvc/web/servlet/frontcontroller/v2/ControllerV2.java
@@ -0,0 +1,13 @@
+package com.example.studyMvc.web.servlet.frontcontroller.v2;
+
+import com.example.studyMvc.web.servlet.frontcontroller.MyView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV2 {
+
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+}
---
+++ studyMvc/src/main/java/com/example/studyMvc/web/servlet/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,27 @@
+package com.example.studyMvc.web.servlet.frontcontroller.v2.controller;
+
+import com.example.studyMvc.domain.member.Member;
+import com.example.studyMvc.domain.member.MemberRepository;
+import com.example.studyMvc.web.servlet.frontcontroller.MyView;
+import com.example.studyMvc.web.servlet.frontcontroller.v2.ControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberListControllerV2.process");
+
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
